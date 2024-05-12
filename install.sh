4ab0caf4716b3240abb9a77a13148dc19beedfd8
011xFrank <138033244+011xFrank@users.noreply.github.com>
2024-05-12 23:52:18 +0300

    mainfiles

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo apt update; sudo apt dist-upgrade
 
 # Install:
-sudo apt install polybar i3 bat git stow curl wget bat neofetch nodejs npm nitrogen eza neofetch vlc tmux neovim
+sudo apt install polybar i3 bat git stow curl wget bat neofetch nodejs npm nitrogen eza neofetch vlc tmux neovim okular cmake
 
 # Installing picom
 sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev libxext-dev meson ninja-build uthash-dev
