22c837d522af2b3e86f29cbb137356fbc5571df5
011xFrank <138033244+011xFrank@users.noreply.github.com>
2024-06-08 16:19:41 +0300

    mainfiles

--- .config/nvim/lua/frank/plugins/lazy.lua
+++ .config/nvim/lua/frank/plugins/lazy.lua
@@ -14,6 +14,8 @@ vim.opt.rtp:prepend(lazypath)
 local options = { ui = { border = "double" } }
 
 local plugins = {
+  {"https://github.com/ggandor/leap.nvim", dependencies = {"tpope/vim-repeat"}},
+
   {"nvim-lualine/lualine.nvim"},
 
   {'brenoprata10/nvim-highlight-colors', config = function() require('nvim-highlight-colors').setup({}) end},
