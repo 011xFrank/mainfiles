16f5eeff81acbb30cb24573e5bb8140094ad70eb
011xFrank <138033244+011xFrank@users.noreply.github.com>
2024-06-28 00:51:23 +0300

    mainfiles

--- .config/nvim/lua/frank/core/options.lua
+++ .config/nvim/lua/frank/core/options.lua
@@ -27,10 +27,10 @@ opt.timeoutlen = 400
 
 -- Indenting
 opt.expandtab = true
-opt.shiftwidth = 4
+opt.shiftwidth = 8
 opt.smartindent = true
-opt.tabstop = 4
-opt.softtabstop = 4
+opt.tabstop = 8
+opt.softtabstop = 8
 
 -- Numbers
 opt.number = true
