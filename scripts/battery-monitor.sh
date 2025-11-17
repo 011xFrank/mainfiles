dc1b8377f8945fbf55956b1d54bedc81ae97a546
011xFrank <138033244+011xFrank@users.noreply.github.com>
2025-11-17 16:26:47 +0300

    refactored battery monitor script to show notification only when the battery is not connected

--- scripts/battery-monitor.sh
+++ scripts/battery-monitor.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 while true; do
-    battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
-    if [ "$battery_level" -le 15 ]; then
-        notify-send "battery low : Plugin Charger"
+    plugin_status=$(cat /sys/class/power_supply/AC/online)
+
+    if [[ "${plugin_status}" == "0" ]]; then
+        battery_level=$(cat /sys/class/power_supply/BAT0/capacity)
+        if [ "$battery_level" -le 15 ]; then
+            notify-send "battery low : Plugin Charger"
+        fi
     fi
+
     sleep 60
 done
